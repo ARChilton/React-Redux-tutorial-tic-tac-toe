502fc3ac93a5ee5ef0f1403609016cb9528a0f50
ARChilton <victorpugliese@Victors-iMac.domain_not_set.invalid>
2018-01-08 12:01:03 +0000

    changed the component to be on a single line as the contents had been deleted - added comments

--- src/components/moves.js
+++ src/components/moves.js
@@ -2,21 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import SingleMove from '../containers/singleMoveContainer'
 
-
+//takes the prop moves and maps it to multiple SingleMove components which are given the move and onClick props
 const Moves = ({ moves, onClick }) => {
     return (
         <ol>
 
             {moves.map((move, id) => (
-                <SingleMove key={id} {...move} onClick={onClick} move={id}>
-
-                </SingleMove>
+                <SingleMove key={id} {...move} onClick={onClick} move={id} />
             ))}
 
         </ol>
     )
 }
-
+//PropTypes checks the props are provided and in the correct type
 Moves.propTypes = {
     moves: PropTypes.array.isRequired,
 
