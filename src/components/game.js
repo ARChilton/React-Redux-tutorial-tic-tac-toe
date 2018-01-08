31a8cb80c71c20fa50e09a8d78502cf99ccf6289
ARChilton <victorpugliese@Victors-iMac.domain_not_set.invalid>
2018-01-08 11:19:19 +0000

    removed commented out previous workings

--- src/components/game.js
+++ src/components/game.js
@@ -1,23 +1,9 @@
 import React from 'react';
-// import { connect } from 'react-redux';
-// import * as actions from '../actions';
-
 
 import Board from './board';
 import GameInfo from './gameInfo'
 
-// function handleClick(i) {
-//     return store.dispatch(actions.clickSquare(i));
-// }
 
-// const mapStateToProps = (state, ownProps) => {
-//     return {
-//         squares: ownProps.squares === state.history[state.history.length - 1].squares
-//     }
-// }
-// const mapDispachToProps = (dispatch, ownProps) => {
-//     onClick: (i) => dispatch(actions.clickSquare(i));
-// }
 
 export const Game = () => {
     return (
